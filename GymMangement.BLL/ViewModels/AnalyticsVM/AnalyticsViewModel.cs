ae72e489d5c0d33d0cf39b10f9e7dab42c50ccb0
MohamedAlaa550 <mohamedalaa.2675@gmail.com>
2025-10-17 21:21:50 +0300

    AnalyticsVN & Service  & View

---
+++ GymMangement.BLL/ViewModels/AnalyticsVM/AnalyticsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymMangement.BLL.ViewModels.AnalyticsVM
+{
+    public class AnalyticsViewModel
+    {
+        public int TotalMembers { get; set; }
+        public int ActiveMembers { get; set; }
+        public int TotalTrainers { get; set; }
+        public int UpCommingSessions { get; set; }
+        public int OnGoingSessions { get; set; }
+        public int ComletedSessions { get; set; }
+
+    }
+}
--- GymMangement.PL/Controllers/HomeController.cs
+++ GymMangement.PL/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using GymMangement.BLL.Services.IntrerFaces;
 using GymMangement.PL.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,15 +8,19 @@ namespace GymMangement.PL.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IAnalyticsService _analyticsService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+            IAnalyticsService analyticsService)
         {
             _logger = logger;
+            _analyticsService = analyticsService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var analyticsData = _analyticsService.GetAnalyticsData();
+            return View(analyticsData);
         }
 
        
